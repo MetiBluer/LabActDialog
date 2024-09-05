d5a487b2185a031584cbeab5763b822e3d109c97
MetiBluer <133645933+MetiBluer@users.noreply.github.com>
2024-09-05 21:26:48 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace LabAct2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.dgvDialog = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbAdd = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importExcelFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDialog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(742, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dgvDialog
+            // 
+            this.dgvDialog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDialog.Location = new System.Drawing.Point(24, 151);
+            this.dgvDialog.Name = "dgvDialog";
+            this.dgvDialog.RowHeadersWidth = 51;
+            this.dgvDialog.RowTemplate.Height = 24;
+            this.dgvDialog.Size = new System.Drawing.Size(658, 326);
+            this.dgvDialog.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(24, 96);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(89, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbAdd
+            // 
+            this.cbAdd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAdd.FormattingEnabled = true;
+            this.cbAdd.Location = new System.Drawing.Point(24, 66);
+            this.cbAdd.Name = "cbAdd";
+            this.cbAdd.Size = new System.Drawing.Size(203, 24);
+            this.cbAdd.TabIndex = 3;
+            this.cbAdd.SelectedIndexChanged += new System.EventHandler(this.cbAdd_SelectedIndexChanged);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(24, 38);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(203, 22);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subjectToolStripMenuItem,
+            this.studentToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // subjectToolStripMenuItem
+            // 
+            this.subjectToolStripMenuItem.Name = "subjectToolStripMenuItem";
+            this.subjectToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.subjectToolStripMenuItem.Text = "Subject";
+            this.subjectToolStripMenuItem.Click += new System.EventHandler(this.subjectToolStripMenuItem_Click);
+            // 
+            // studentToolStripMenuItem
+            // 
+            this.studentToolStripMenuItem.Name = "studentToolStripMenuItem";
+            this.studentToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.studentToolStripMenuItem.Text = "Student";
+            this.studentToolStripMenuItem.Click += new System.EventHandler(this.studentToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importExcelFileToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // importExcelFileToolStripMenuItem
+            // 
+            this.importExcelFileToolStripMenuItem.Name = "importExcelFileToolStripMenuItem";
+            this.importExcelFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.importExcelFileToolStripMenuItem.Text = "Import  Excel File";
+            this.importExcelFileToolStripMenuItem.Click += new System.EventHandler(this.importExcelFileToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 521);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cbAdd);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvDialog);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Excel Importing App";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDialog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.DataGridView dgvDialog;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbAdd;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importExcelFileToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabAct2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void importExcelFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile.Title = "Open Excel";
+            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFile.Filter = "All files (*.*)|*.*|Excel File (*.xlxs)|*.xlsx";
+            openFile.FilterIndex = 2;
+            openFile.ShowDialog();
+
+            if (!string.IsNullOrEmpty(openFile.Filter))
+            {
+                OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                    "Data Source=" + openFile.FileName + "; Extended Properties='Excel 12.0 Xml; HDR=Yes'");
+                conn.Open();
+                OleDbDataAdapter ada = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", conn);
+                DataTable dt = new DataTable();
+                ada.Fill(dt);
+                dgvDialog.DataSource = dt;
+                conn.Close();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+      
+                if (cbAdd.SelectedIndex == 0)
+                {
+                    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                       "Data Source=" + openFile.FileName + "; Extended Properties='Excel 12.0 Xml; HDR=Yes'");
+                    string query = "SELECT * FROM [Sheet1$] WHERE idNumber LIKE '" + txtSearch.Text + "' ";
+                    OleDbCommand cmd = new OleDbCommand(query, conn);
+                    OleDbDataAdapter ada = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    ada.Fill(dt);
+                    dgvDialog.DataSource = dt;
+                    conn.Close();
+                }
+               else if (cbAdd.SelectedIndex == 1)
+                {
+                    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                       "Data Source=" + openFile.FileName + "; Extended Properties='Excel 12.0 Xml; HDR=Yes'");
+                    string query = "SELECT * FROM [Sheet2$] WHERE code LIKE '" + txtSearch.Text + "' ";
+                    OleDbCommand cmd = new OleDbCommand(query, conn);
+                    OleDbDataAdapter ada = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    ada.Fill(dt);
+                    dgvDialog.DataSource = dt;
+                    conn.Close();
+
+                }
+                else
+            {
+                MessageBox.Show("Select a Data");
+            }
+
+        }
+
+        private void cbAdd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cbAdd.Items.Add("Student");
+            cbAdd.Items.Add("Subject");
+
+        }
+
+        private void subjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmSubject frmSub = new frmSubject();
+            frmSub.ShowDialog();
+        }
+
+        private void studentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmStudent frmStud = new frmStudent();
+            frmStud.ShowDialog();
+        }
+    }
+}
